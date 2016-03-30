2ce113db9d38efcb7286689adae846d5c4fa3eba
Colin Wood <cwood06@gmail.com>
2016-03-29 17:22:05 -0700

    Add support for a Note.
    This will prepend the script with a the #=- to setup the note in the UI

--- file.go
+++ file.go
@@ -1,18 +1,22 @@
 package stingray
 
+import (
+	"fmt"
+)
+
 // fileResource represents a file resource.
 type fileResource struct {
 	resource
 	Content []byte
+	Note    string
 }
 
 func (f *fileResource) String() string {
-	return string(f.Content)
+	return fmt.Sprintf("#=-%v\n", f.Note) + string(f.Content)
 }
 
 func (f *fileResource) decode(data []byte) error {
 	f.Content = data
-
 	return nil
 }
 
