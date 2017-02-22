1851e2bf82f539fd2993d79eb1fd5336a7d42e7f
Colin Wood <cwood06@gmail.com>
2017-02-22 13:57:31 -0800

    Remove manging the file resource for a note/script/file

--- file.go
+++ file.go
@@ -1,11 +1,5 @@
 package stingray
 
-import (
-	"fmt"
-	"log"
-	"strings"
-)
-
 // fileResource represents a file resource.
 type fileResource struct {
 	resource
@@ -14,21 +8,7 @@ type fileResource struct {
 }
 
 func (f *fileResource) String() string {
-	if f.GetNote() == "" {
-		return fmt.Sprintf("#=-%v\n", f.Note) + string(f.Content)
-	} else {
-		return string(f.Content)
-	}
-}
-
-func (f *fileResource) GetNote() string {
-	firstln := strings.Split(string(f.Content), "\n")[0]
-	if strings.Contains(firstln, "#=-") {
-		log.Printf("[DEBUG] Found note in resource: %s", firstln)
-		return string([]rune(firstln)[3:])
-	}
-	log.Printf("[DEBUG] Did not find note in resource: %s", firstln)
-	return f.Note
+	return string(f.Content)
 }
 
 func (f *fileResource) decode(data []byte) error {
